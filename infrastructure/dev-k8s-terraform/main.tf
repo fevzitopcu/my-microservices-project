22d3bd826ac1b5d22bacea6db09e1007e5881c2d
fevzitopcu <fevzitopcu@gmail.com>
2022-06-28 22:18:06 +0000

    ok

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -135,7 +135,7 @@ resource "aws_security_group" "matt-kube-master-sg" {
 resource "aws_instance" "kube-master" {
     ami = "ami-013f17f36f8b1fefb"
     instance_type = "t3a.medium"
-    iam_instance_profile = module.iam.master_profile_name
+    iam_instance_profile = "module.iam.master_profile_name"
     vpc_security_group_ids = [aws_security_group.matt-kube-master-sg.id, aws_security_group.matt-kube-mutual-sg.id]
     key_name = "mattkey"  # job çalıştırırken değiştirilecek
     subnet_id = "subnet-0470b583b72022b6a"  # select own subnet_id of us-east-1a
@@ -153,7 +153,8 @@ resource "aws_instance" "kube-master" {
 resource "aws_instance" "worker-1" {
     ami = "ami-013f17f36f8b1fefb"
     instance_type = "t3a.medium"
-    iam_instance_profile = module.iam.worker_profile_name
+    iam_instance_profile = "module.iam.worker_profile_name"
+
     vpc_security_group_ids = [aws_security_group.matt-kube-worker-sg.id, aws_security_group.matt-kube-mutual-sg.id]
     key_name = "mattkey" # job çalıştırırken değiştirilecek
     subnet_id = "subnet-0470b583b72022b6a"  # select own subnet_id of us-east-1a
@@ -171,7 +172,7 @@ resource "aws_instance" "worker-1" {
 resource "aws_instance" "worker-2" {
     ami = "ami-013f17f36f8b1fefb"
     instance_type = "t3a.medium"
-    iam_instance_profile = module.iam.worker_profile_name
+    iam_instance_profile = "module.iam.worker_profile_name"
     vpc_security_group_ids = [aws_security_group.matt-kube-worker-sg.id, aws_security_group.matt-kube-mutual-sg.id]
     key_name = "mattkey"  # job çalıştırırken değiştirilecek
     subnet_id = "subnet-0470b583b72022b6a"  # select own subnet_id of us-east-1a
--- infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
+++ infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
@@ -68,20 +68,20 @@ resource "aws_iam_policy_attachment" "attach_for_worker" {
   policy_arn = aws_iam_policy.policy_for_worker_role.arn
 }
 
-resource "aws_iam_instance_profile" "profile_for_master-1" {
-  name  = "profile_for_master-1"
+resource "aws_iam_instance_profile" "profile_for_master" {
+  name  = "profile_for_master"
   role = aws_iam_role.role_for_master.name
 }
 
-resource "aws_iam_instance_profile" "profile_for_worker-1" {
-  name  = "profile_for_worker-1"
+resource "aws_iam_instance_profile" "profile_for_worker" {
+  name  = "profile_for_worker"
   role = aws_iam_role.role_for_worker.name
 }
 
 output master_profile_name {
-  value       = aws_iam_instance_profile.profile_for_master-1.name
+  value       = aws_iam_instance_profile.profile_for_master.name
 }
 
 output worker_profile_name {
-  value       = aws_iam_instance_profile.profile_for_worker-1.name
+  value       = aws_iam_instance_profile.profile_for_worker.name
 }
