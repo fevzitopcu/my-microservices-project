4ec3327f1d5d48f3ced1911791901de332fc99ad
fevzitopcu <fevzitopcu@gmail.com>
2022-06-28 21:20:13 +0000

    ok

--- infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
+++ infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
@@ -69,12 +69,12 @@ resource "aws_iam_policy_attachment" "attach_for_worker" {
 }
 
 resource "aws_iam_instance_profile" "profile_for_master" {
-  name  = "profile_for_master"
+  name  = "profile_for_masterr"
   role = aws_iam_role.role_for_master.name
 }
 
 resource "aws_iam_instance_profile" "profile_for_worker" {
-  name  = "profile_for_worker"
+  name  = "profile_for_workerr"
   role = aws_iam_role.role_for_worker.name
 }
 
