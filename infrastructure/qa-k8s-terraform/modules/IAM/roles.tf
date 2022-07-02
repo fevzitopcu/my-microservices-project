2d6d6c3a33b2563e489563cbdbfc491c0476595a
fevzitopcu <fevzitopcu@gmail.com>
2022-07-02 13:02:37 +0000

    iam role edit

--- infrastructure/qa-k8s-terraform/modules/IAM/roles.tf
+++ infrastructure/qa-k8s-terraform/modules/IAM/roles.tf
@@ -60,28 +60,23 @@ resource "aws_iam_policy_attachment" "attach_for_master" {
   name       = "attachment_for_master"
   roles      = [aws_iam_role.role_for_master.name]
   policy_arn = aws_iam_policy.policy_for_master_role.arn
-  depends_on = [
-    aws_iam_role.role_for_master
-  ]
+
 }
 
 resource "aws_iam_policy_attachment" "attach_for_worker" {
   name       = "attachment_for_worker"
   roles      = [aws_iam_role.role_for_worker.name]
   policy_arn = aws_iam_policy.policy_for_worker_role.arn
-  depends_on = [
-    aws_iam_role.role_for_worker
-  ]
-}
+
 
 resource "aws_iam_instance_profile" "profile_for_master" {
-  name  = "profile_for_master"
+  # name  = "profile_for_master"
   role = aws_iam_role.role_for_master.name
   
 }
 
 resource "aws_iam_instance_profile" "profile_for_worker" {
-  name  = "profile_for_worker"
+  # name  = "profile_for_worker"
   role = aws_iam_role.role_for_worker.name
 }
 
